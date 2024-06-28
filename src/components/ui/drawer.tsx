bbf654e89d2f97e3a145795f2a8b6b43eb4eff81
rasthpop <rasthetrollman@example.com>
2024-06-28 18:35:26 +0300

    135

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -7,7 +7,7 @@ import { Drawer as DrawerPrimitive } from "vaul"
 import { cn } from "@/lib/utils"
 
 const Drawer = ({
-  shouldScaleBackground = true,
+  shouldScaleBackground = false,
   ...props
 }: React.ComponentProps<typeof DrawerPrimitive.Root>) => (
   <DrawerPrimitive.Root
