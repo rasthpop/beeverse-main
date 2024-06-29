4bd9e4cde9a4eac1a50450669f0697ddead4bf25
rasthpop <rasthetrollman@example.com>
2024-06-29 18:27:40 +0300

    1641

--- src/components/referral-reward-card.tsx
+++ src/components/referral-reward-card.tsx
@@ -12,14 +12,14 @@ export default function ReferralRewardCard(
     }
 ) {
     return(
-        <div className="w-full min-h-fit h-[90px] bg-backdrop border-2 border-border rounded-2xl inline-flex gap-2 items-center justify-between p-2">
+        <div className="w-full min-h-fit h-[75px] bg-backdrop border-2 border-border rounded-2xl inline-flex gap-2 items-center justify-between p-1">
             <div className="h-full inline-flex items-center justify-center p-2">
-                <Avatar className="w-14 h-14">
+                <Avatar className="w-10 h-10">
                     <AvatarImage
                     src={props.avatarURL}
                     alt="avatar"
-                    width={128}
-                    height={128}
+                    width={96}
+                    height={96}
                     />
                     <AvatarFallback className="bg-hover">?</AvatarFallback>
                 </Avatar>
@@ -27,8 +27,8 @@ export default function ReferralRewardCard(
             <div className="w-full h-full inline-flex gap-1 items-center justify-between">
 				<div className="w-full max-w-32 h-full flex flex-col items-start justify-start">
 					<HoneyDisplay isBold={false} amount={props.amount} iconSize={24} />
-					<div className="w-full h-full inline-flex items-end justify-start">
-						<p className="text-base leading-5 text-foreground font-medium text-normal-stroke">{props.title}</p>
+					<div className="w-full h-full inline-flex items-start justify-start">
+						<p className="text-[14px] leading-5 text-foreground font-medium text-normal-stroke">{props.title}</p>
 					</div>
 				</div>
 				<div className="h-full inline-flex items-center justify-end">
