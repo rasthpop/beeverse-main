e91967670f2c8e39d5ef1fc2afa9e525ce9717ba
rasthpop <rasthetrollman@example.com>
2024-06-30 16:54:58 +0300

    293

--- src/components/ui/rankprogress.tsx
+++ src/components/ui/rankprogress.tsx
@@ -16,18 +16,18 @@ export default function Rankprogress(props:{
 	}, [props.balance, props.reqAmount]);
 
     return(
-		<div className='relative w-[240px] h-[26px] my-2'>
+		<div className='relative w-[240px] h-[26px] my-2 min-[399px]:w-[280px]'>
 				<Image src={'/progressbar/hp-bar-empty.png'} fill objectFit='cover' alt='Hp bar Empty' />
 				<div
 					style={{ width: healthBarWidth + '%' }}
 					className={`max-w-[89%] min-w-[11%] h-[26px]overflow-hidden`}>
 					<img
-						className='w-[240px] relative max-w-none top-[-3px]'
+						className='w-[240px] relative max-w-none top-[-3px] min-[399px]:w-[280px] min-[399px]:top-[-6px]'
 						src={'/progressbar/hp-bar-full.png'}
 						alt='Hp bar Full'
 					/>
 				</div>
-				<h2 className='absolute top-[6px] text-[#ECEBEA] text-[10px] font-medium text-center w-full'>
+				<h2 className='absolute top-[6px] text-[#ECEBEA] text-[10px] font-medium text-center w-full min-[399px]:top-[5px]'>
 					{props.balance}/{props.reqAmount} HP
 				</h2>
 			</div>        
